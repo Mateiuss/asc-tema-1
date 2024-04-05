9d1335d3db27bb204094f1fa916adbd7143a46f9
Matei-Ioan DUDU <mateiioan.dudu@gmail.com>
2024-04-05 19:32:30 +0300

    Removed some unnecessary comments in the task_runner.py file

--- app/task_runner.py
+++ app/task_runner.py
@@ -1,19 +1,10 @@
 from queue import Queue
 from threading import Thread, Lock, Semaphore
-import time
 import os
 import json
 
 class ThreadPool:
     def __init__(self, logger):
-        # You must implement a ThreadPool of TaskRunners
-        # Your ThreadPool should check if an environment variable TP_NUM_OF_THREADS is defined
-        # If the env var is defined, that is the number of threads to be used by the thread pool
-        # Otherwise, you are to use what the hardware concurrency allows
-        # You are free to write your implementation as you see fit, but
-        # You must NOT:
-        #   * create more threads than the hardware concurrency allows
-        #   * recreate threads for each task
         if 'TP_NUM_OF_THREADS' in os.environ:
             self.num_threads = os.environ.get('TP_NUM_OF_THREADS')
         else:
@@ -72,7 +63,6 @@ def key_to_string(data: dict) -> dict:
 
 class TaskRunner(Thread):
     def __init__(self, thread_pool: ThreadPool):
-        # TODO: init necessary data structures
         Thread.__init__(self)
         self.thread_pool = thread_pool
 
@@ -85,11 +75,6 @@ class TaskRunner(Thread):
 
     def run(self):
         while True:
-            # TODO
-            # Get pending job
-            # Execute the job and save the result to disk
-            # Repeat until graceful_shutdown
-
             # Wait for a job to be available
             (job_id, request_json, work) = self.get_job()
 
