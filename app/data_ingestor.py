4500b50bebdb705c615011c49d1c20957ce1495a
Matei-Ioan DUDU <mateiioan.dudu@gmail.com>
2024-04-07 15:11:23 +0300

    Made data ingestor to work with pylint

--- app/data_ingestor.py
+++ app/data_ingestor.py
@@ -1,13 +1,21 @@
+"""
+This is the module for the DataIngestor class.
+The class is explained below.
+"""
+
 import csv
 
 class DataIngestor:
+    """
+    This is the class that will be used to ingest the data from the CSV file and
+    provide the necessary methods to get the required information.
+    """
     def __init__(self, csv_path: str):
-        # TODO: Read csv from csv_path
-        with open(csv_path, 'r')as file:
-            csvFile = csv.DictReader(file)
+        with open(csv_path, 'r', encoding='utf-8')as file:
+            csv_file = csv.DictReader(file)
             self.data = []
 
-            for line in csvFile:
+            for line in csv_file:
                 self.data.append(line)
 
         self.questions_best_is_min = [
@@ -19,25 +27,40 @@ class DataIngestor:
         ]
 
         self.questions_best_is_max = [
-            'Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)',
-            'Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic physical activity and engage in muscle-strengthening activities on 2 or more days a week',
-            'Percent of adults who achieve at least 300 minutes a week of moderate-intensity aerobic physical activity or 150 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)',
-            'Percent of adults who engage in muscle-strengthening activities on 2 or more days a week',
+            'Percent of adults who achieve at least 150 minutes a week of moderate-intensity '\
+            'aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic '\
+            'activity (or an equivalent combination)',
+            'Percent of adults who achieve at least 150 minutes a week of moderate-intensity '\
+            'aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic '\
+            'physical activity and engage in muscle-strengthening activities on 2 '\
+            'or more days a week',
+            'Percent of adults who achieve at least 300 minutes a week of moderate-intensity '\
+            'aerobic physical activity or 150 minutes a week of vigorous-intensity aerobic '\
+            'activity (or an equivalent combination)',
+            'Percent of adults who engage in muscle-strengthening activities on 2 or more '\
+            'days a week',
         ]
 
     def state_mean(self, request_json):
+        """
+        Function that gets the state mean for a given state and question.
+        """
         state = request_json['state']
         question = request_json['question']
-        state_data = [row for row in self.data if row['LocationDesc'] == state and row['Question'] == question]
+        state_data = [row for row in self.data if row['LocationDesc'] == state and
+                       row['Question'] == question]
 
         if len(state_data) == 0:
             return None
-        
-        return {state: sum([float(row['Data_Value']) for row in state_data]) / len(state_data)}
-    
+
+        return {state: sum((float(row['Data_Value']) for row in state_data)) / len(state_data)}
+
     def states_mean(self, request_json):
+        """
+        Function that gets the mean of all states for a given question.
+        """
         question = request_json['question']
-        states = set([row['LocationDesc'] for row in self.data if row['Question'] == question])
+        states = {row['LocationDesc'] for row in self.data if row['Question'] == question}
 
         states_mean = {}
 
@@ -50,33 +73,47 @@ class DataIngestor:
         return states_mean
 
     def best5(self, request_json):
+        """
+        Function that gets the best 5 states for a given question.
+        """
         question = request_json['question']
         best_is_max = question in self.questions_best_is_max
-        
+
         states_mean = self.states_mean(request_json)
         sorted_states = sorted(states_mean.items(), key=lambda x: x[1], reverse=best_is_max)
 
         return dict(sorted_states[:5])
-    
+
     def worst5(self, request_json):
+        """
+        Functions that gets the worst 5 states for a given question.
+        """
         question = request_json['question']
         best_is_min = question in self.questions_best_is_min
-        
+
         states_mean = self.states_mean(request_json)
         sorted_states = sorted(states_mean.items(), key=lambda x: x[1], reverse=best_is_min)
 
         return dict(sorted_states[:5])
-    
+
     def global_mean(self, request_json):
+        """
+        Function that gets the global mean for a given question.
+        """
         question = request_json['question']
         question_data = [row for row in self.data if row['Question'] == question]
 
         if len(question_data) == 0:
             return None
-        
-        return {'global_mean': sum([float(row['Data_Value']) for row in question_data]) / len(question_data)}
-    
+
+        return {'global_mean': sum((float(row['Data_Value'])
+                                    for row in question_data)) / len(question_data)}
+
     def state_diff_from_mean(self, request_json):
+        """
+        Function that gets the difference between the global mean and the state mean for a given
+        state and question.
+        """
         question = request_json['question']
         state = request_json['state']
 
@@ -87,8 +124,12 @@ class DataIngestor:
         state_mean = state_mean[state]
 
         return {state: global_mean - state_mean}
-    
+
     def diff_from_mean(self, request_json):
+        """
+        Function that gets the difference between the global mean and the state mean on a given
+        question for all states.
+        """
         question = request_json['question']
         global_mean = self.global_mean({'question': question})
         global_mean = global_mean['global_mean']
@@ -98,6 +139,9 @@ class DataIngestor:
         return {state: global_mean - state_mean for state, state_mean in states_mean.items()}
 
     def state_mean_by_category(self, request_json):
+        """
+        Function that gets the mean of state for a given question and state by category.
+        """
         question = request_json['question']
         state = request_json['state']
 
@@ -120,28 +164,32 @@ class DataIngestor:
                 else:
                     stratification_categories[category][stratification].append(row['Data_Value'])
 
-        for category in stratification_categories:
-            for stratification in stratification_categories[category]:
-                stratification_categories[category][stratification] = sum([float(x) for x in stratification_categories[category][stratification]]) / len(stratification_categories[category][stratification])
+        for (category, value) in stratification_categories.items():
+            for stratification in value:
+                value[stratification] =\
+                sum((float(x) for x in value[stratification])) / len(value[stratification])
 
         ans = {}
 
-        for category in stratification_categories:
-            for stratification in stratification_categories[category]:
-                ans[(category, stratification)] = stratification_categories[category][stratification]
+        for (category, value) in stratification_categories.items():
+            for stratification in value:
+                ans[(category, stratification)] = value[stratification]
 
         return {state: ans}
-    
+
     def mean_by_category(self, request_json):
+        """
+        Function that gets the mean of all states for a given question by category.
+        """
         question = request_json['question']
-        states = set([row['LocationDesc'] for row in self.data if row['Question'] == question])
+        states = {row['LocationDesc'] for row in self.data if row['Question'] == question}
 
         ans = {}
 
         for state in states:
             request = {'state': state, 'question': question}
             state_mean_by_category = self.state_mean_by_category(request)
-            
+
             for (category, stratification), value in state_mean_by_category[state].items():
                 ans[(state, category, stratification)] = value
 
